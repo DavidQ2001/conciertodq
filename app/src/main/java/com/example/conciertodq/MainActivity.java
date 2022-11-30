09f75b260a89e9ba3fdb0fd8e5261c3b5bf57faa
Juanproyect <118578519+Juanproyect@users.noreply.github.com>
2022-11-30 17:37:12 -0500

    ejerciciomoviles2

--- app/src/main/java/com/example/conciertodq/MainActivity.java
+++ app/src/main/java/com/example/conciertodq/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.conciertodq;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText jetnombre,jetcedula;
+    CheckBox jcbvip,jcbprefencial,jcbgeneral,jcbactivo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jetnombre=findViewById(R.id.etnombre);
+        jetcedula=findViewById(R.id.etcedula);
+        jcbactivo=findViewById(R.id.cbactivo);
+        jcbgeneral=findViewById(R.id.cbgeneral);
+        jcbprefencial=findViewById(R.id.cbpreferencial);
+        jcbvip=findViewById(R.id.cbvip);
+
+
     }
+
+
 }
